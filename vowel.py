58b06a0893cb4fc10716baf3a6552f79483cb83f
GRALINA <37367209+GRALINA@users.noreply.github.com>
2018-03-14 09:22:55 -0700

    vowel
    whether a character is vowel or not

---
+++ vowel.py
@@ -0,0 +1,7 @@
+cha = input("Enter a character: ")
+
+if(cha=='A' or cha=='a' or cha=='E' or cha =='e' or cha=='I'
+ or cha=='i' or cha=='O' or cha=='o' or cha=='U' or cha=='u'):
+    print(cha, "is a Vowel")
+else:
+    print(cha, "is a Consonant")
